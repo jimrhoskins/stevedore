e36c6b00c78a715766a1b6fc952352cee4de3334
Jim Hoskins <jim@jimhoskins.com>
2013-11-10 02:53:21 +0000

    mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,16 @@
 Stevedore::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  #
+
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yourapp.heroku.com',
+    :authentication => :plain,
+  }
+  config.action_mailer.delivery_method = :smtp
 
   # Code is not reloaded between requests.
   config.cache_classes = true
