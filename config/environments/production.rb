edce92c6ba6105ce90e2df290a8ea50219a49cec
Jim Hoskins <jim@jimhoskins.com>
2013-11-10 02:57:28 +0000

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Stevedore::Application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
+    :domain         => 'stevedore-treehouse.herokuapp.com',
     :authentication => :plain,
   }
   config.action_mailer.delivery_method = :smtp
